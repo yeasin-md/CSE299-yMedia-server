74540c8c97b056aad0c1863411491c6737962c89
Md. Yeasin Hossain <80535880+yeasin-md@users.noreply.github.com>
2025-12-08 03:00:39 +0600

    Delete server directory

--- server/models/Categories.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const CategorySchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  img: { type: String },
-});
-
-module.exports = mongoose.model("Category", CategorySchema);
